c38a53669c17e23bf9320fc7aec597904daef7f2
jatin30032001 <jrsaini2090@gmail.com>
2023-03-25 01:09:27 +0530

    First Commit

---
+++ client/netflix/src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+// import Navbar from "../../components/navbar/Navbar";
+// import Featured from "../../components/featured/Featured";
+import "./home.scss";
+
+// import List from "../../components/list/List";
+
+const Home = () => {
+  return (
+    <div className="home">
+        Hell WEL come
+        
+      {/* <Navbar />
+      <Featured/>
+      <List/>
+      <List/>
+      <List/>
+      <List/> */}
+    </div>
+  );
+};
+
+export default Home;
