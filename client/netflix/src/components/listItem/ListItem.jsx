2b64eb2cf1c51ed70072198b15c5fdf8bc6beaf2
jatin30032001 <jrsaini2090@gmail.com>
2023-03-26 00:08:18 +0530

    Slider Commit

---
+++ client/netflix/src/components/listItem/ListItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./listItem.scss"
+
+
+const ListItem = () => {
+  return (
+   <div className="listItem">
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
+   </div>
+  )
+}
+
+export default ListItem
