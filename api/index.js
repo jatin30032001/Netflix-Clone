c510165461731888bc88c338040520ca2b7a771e
jatin30032001 <jrsaini2090@gmail.com>
2023-03-28 11:58:20 +0530

    Login and Register Commit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+
+dotenv.config();
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  
+  })
+  .then(()=>{
+    console.log("DB connection is successful");
+  })
+  .catch((e)=>{
+    console.log(e);
+  })
+
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+
+
+app.listen("8800",()=>{
+    console.log("Backend is Running");
+})
